cdb04552fb1b44e098f6c82e7211ab22a7399709
Paul Trampert <paul.trampert@rackspace.com>
2015-10-28 10:00:26 -0400

    Extracted interface for easier unit testing and dependency injection.

--- Rackspace.CloudOffice/ApiClient.cs
+++ Rackspace.CloudOffice/ApiClient.cs
@@ -13,7 +13,23 @@ using Newtonsoft.Json;
 
 namespace Rackspace.CloudOffice
 {
-    public class ApiClient
+    public interface IApiClient
+    {
+        string BaseUrl { get; }
+        string UserKey { get; }
+        IDictionary<string, string> CustomHeaders { get; }
+        Task<dynamic> Get(string path);
+        Task<T> Get<T>(string path);
+        Task<IEnumerable<dynamic>> GetAll(string path, string pagedProperty, int pageSize = 50);
+        Task<IEnumerable<T>> GetAll<T>(string path, string pagedProperty, int pageSize = 50);
+        Task<dynamic> Post(string path, object data, string contentType=ApiClient.ContentType.UrlEncoded);
+        Task<T> Post<T>(string path, object data, string contentType=ApiClient.ContentType.UrlEncoded);
+        Task<dynamic> Put(string path, object data, string contentType=ApiClient.ContentType.UrlEncoded);
+        Task<T> Put<T>(string path, object data, string contentType=ApiClient.ContentType.UrlEncoded);
+        Task Delete(string path);
+    }
+
+    public class ApiClient : IApiClient
     {
         public const string DefaultBaseUrl = "https://api.emailsrvr.com";
 
